d2edc456a311ddbbfd902ef48cc1221b60003785
Christoph Heindl <christoph.heindl@gmail.com>
2017-02-23 17:43:00 +0100

    moving file.

--- robots/robot.py
+++ robots/robot.py
@@ -7,20 +7,30 @@ import math
 def default_state():
     return np.array([0.,0.,0.])
 
-def move(state, params):
+def move(state, motion, err_motion=[0., 0.], inplace=False):
     """Move robot by first turning and then driving along the robot's x-axis."""
-    phi = state[2] + params[0]
-    return np.array([
-        state[0] + math.cos(phi) * params[1],
-        state[1] + math.sin(phi) * params[1],
-        phi
-    ])
+    e_phi = 0. if err_motion[0] == 0. else np.random.normal(scale=err_motion[0])
+    e_delta = 0. if err_motion[1] == 0. else np.random.normal(scale=err_motion[1])
+
+    if inplace:
+        state[2] += motion[0] + e_phi
+        state[0] += math.cos(state[2]) * (motion[1] + e_delta)
+        state[1] += math.sin(state[2]) * (motion[1] + e_delta)
+        return state
+    else:
+        phi = state[2] + motion[0] + e_phi
+        return np.array([
+            state[0] + math.cos(phi) * (motion[1] + e_delta),
+            state[1] + math.sin(phi) * (motion[1] + e_delta),
+            phi
+        ])
 
     # Angle wrap
     # math.atan2(math.sin(a), math.cos(a)) -> -pi..pi
     # math.atan2(math.sin(a-math.pi), math.cos(a-math.pi)) + math.pi -> 0..2pi
 
 def robot_to_world(state):
+    """Returns a 2x3 matrix representing the robot's location in world space."""
     c = math.cos(state[2])
     s = math.sin(state[2])
 
@@ -30,6 +40,7 @@ def robot_to_world(state):
     ])
 
 def world_to_robot(state):
+    """Returns a 2x3 matrix representing the world in robot space."""
     c = math.cos(state[2])
     s = math.sin(state[2])
 
@@ -38,7 +49,7 @@ def world_to_robot(state):
         [-s, c, -(-s * state[0] + c * state[1])]
     ])
 
-def draw(state, ax, radius=0.5):
+def draw(state, ax, radius=0.5, fc='None', ec='k'):
 
     m = robot_to_world(state)
     o = np.dot(m, [0,0,1])
@@ -48,7 +59,7 @@ def draw(state, ax, radius=0.5):
     dx = math.cos(state[2]) * radius
     dy = math.sin(state[2]) * radius
 
-    c = plt.Circle(state[:2], radius=radius, fc='none', ec='k')
+    c = plt.Circle(state[:2], radius=radius, fc=fc, ec=ec)
     ax.add_artist(c)
 
     lx, = ax.plot([o[0], x[0]], [o[1], x[1]], color='r')
@@ -64,11 +75,10 @@ ax.set_aspect('equal')
 r = np.zeros(3)
 
 def update(i):    
-    global r
-    r = move(r, [0.05, 0.1])
+    move(r, [0.05, 0.1], err_motion=[0.001, 0.01], inplace=True)
     
     e = []
-    e.extend(draw(r, ax))
+    e.extend(draw(r, ax, fc='b', ec='None'))
     return e
 
 

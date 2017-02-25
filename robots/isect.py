f5a02b3d469d622d6969c6313545cca5f1c5b360
Christoph Heindl <christoph.heindl@gmail.com>
2017-02-25 10:33:37 +0100

    more tests.

--- robots/isect.py
+++ robots/isect.py
@@ -68,59 +68,7 @@ def ray_grid(o, d, bounds, shape, hitmask=None, hitfnc=None):
     return False, t, cell
 
 
-
-# class Grid:
-
-#     def __init__(self, shape, mincorner=[0.,0.], cellsize=[1,1], dtype=None):
-#         self.cells = np.zeros(shape, dtype=dtype)
-#         self.mincorner = np.asarray(mincorner, dtype=float)
-#         self.cellsize = np.asarray(cellsize, dtype=float)
-
-#     def cell_index(self, x):
-#         """Returns cell indices for one or more world space points."""
-#         return np.floor((np.asarray(x) - self.mincorner) / self.cellsize).astype(int)
-
-#     def index_inside(self, idx):
-#         """Tests if one or more cell indices are contained within the bounds of the grid."""
-#         idx = np.asarray(idx)
-#         b = np.logical_and(idx >= (0,0), idx < self.cells.shape)
-#         return np.all(b, axis=1)
-
-#     def trace_ray(self, origin, direction):
-#         # http://lodev.org/cgtutor/raycasting.html
-
-
-#         ogrid = origin - self.mincorner
-#         delta = np.zeros(2)
-#         step = np.zeros(2)
-
-#         if direction[0] < 0.:
-#             delta[0] = -self.cells.shape[0] / direction[0]
-#             step[0] = (np.floor(ogrid[0] / self.cellsize[0]) * self.cellsize[0] - ogrid[0]) / direction[0]
-#         else:
-#             delta[0] = self.cells.shape[0] / direction[0]
-#             step[0] = ((np.floor(ogrid[0] / self.cellsize[0]) + 1.) * self.cellsize[0] - ogrid[0]) / direction[0]
-
-#         if direction[1] < 0.:
-#             delta[1] = -self.cells.shape[1] / direction[1]
-#             step[1] = (np.floor(ogrid[1] / self.cellsize[1]) * self.cellsize[1] - ogrid[1]) / direction[1]
-#         else:
-#             delta[1] = self.cells.shape[1] / direction[1]
-#             step[1] = ((np.floor(ogrid[1] / self.cellsize[1]) + 1.) * self.cellsize[1] - ogrid[1]) / direction[1]
-
-#         print(step)
-#         print(origin + step[0] * direction)
-#         print(origin + step[1] * direction)
-        
-# g = Grid((10,10), cellsize=(0.5, 0.5))
-# print(g.cell_index(np.array([
-#     [0,0],
-#     [1,2]
-# ])))
-
-# print(g.index_inside(((0,0), (-0.5,0.0))))
-
-
+"""
 print(
     ray_box(np.array([-2, 0]), np.array([1, 0]), np.array([[0,0],[10,10]]))
 )
@@ -142,12 +90,6 @@ dirs /= np.linalg.norm(dirs, axis=1)[:, np.newaxis]
 
 for i in range(dirs.shape[0]):
     o = np.array([5, 5])
-    
-    """r = ray_grid(
-        o,
-        dirs[i],
-        bounds,
-        mask)"""
 
     def myhit(o, d, t, cell):
         p = o + t * d
@@ -176,3 +118,4 @@ plt.show()
 #print(r)
 
 #plt.show()
+"""
--- robots/tests/test_isect.py
+++ robots/tests/test_isect.py
@@ -35,3 +35,62 @@ def test_ray_box():
     assert ret
     assert tbox == approx(0.)
     assert tboxexit == approx(math.sqrt(5**2+5**2))
+
+
+def test_ray_grid():
+    
+    bounds = np.array([[0,0],[10,10]])
+    mask = np.zeros((10, 10))
+    mask[:, -1] = 1.
+    mask[:, 0] = 1.
+
+    o = np.array([-2, 0])
+    d = np.array([1, 0])
+    ret, thit, cell = isect.ray_grid(o, d, bounds, mask.shape, hitmask=mask)
+    assert ret
+    assert thit == approx(2)
+    assert (cell == [0,0]).all()
+
+    o = np.array([0, 0])
+    d = np.array([1, 0])
+    ret, thit, cell = isect.ray_grid(o, d, bounds, mask.shape, hitmask=mask)
+    assert ret
+    assert thit == approx(0)
+    assert (cell == [0,0]).all()
+
+    o = np.array([2.2, 0])
+    d = np.array([1, 0])
+    ret, thit, cell = isect.ray_grid(o, d, bounds, mask.shape, hitmask=mask)
+    assert ret
+    assert thit == approx(7-0.2)
+    assert (cell == [9,0]).all()
+
+    o = np.array([2.2, 0])
+    d = np.array([-1, 0])
+    ret, thit, cell = isect.ray_grid(o, d, bounds, mask.shape, hitmask=mask)
+    assert ret
+    assert thit == approx(1.2)
+    assert (cell == [0,0]).all()
+
+    o = np.array([2.2, 0])
+    d = np.array([0, 1])
+    ret, thit, cell = isect.ray_grid(o, d, bounds, mask.shape, hitmask=mask)
+    assert not ret
+
+    o = np.array([-2.2, 0])
+    d = np.array([0, 1])
+    ret, thit, cell = isect.ray_grid(o, d, bounds, mask.shape, hitmask=mask)
+    assert not ret
+
+    o = np.array([0, 0])
+    d = np.array([0, 1])
+    ret, thit, cell = isect.ray_grid(o, d, bounds, mask.shape, hitmask=mask)
+    assert ret
+
+    o = np.array([2, 2])
+    d = np.array([1, 1])
+    d = d / np.linalg.norm(d)
+    ret, thit, cell = isect.ray_grid(o, d, bounds, mask.shape, hitmask=mask)
+    assert ret
+    assert thit == approx(math.sqrt(7*7 + 7*7))
+    assert (cell == [9,9]).all()

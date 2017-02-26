cea9e901b4b9e1af02e60f569877c719b06bda23
Christoph Heindl <christoph.heindl@gmail.com>
2017-02-26 14:26:28 +0100

    support for occluded landmarks

--- robots/sensors.py
+++ robots/sensors.py
@@ -10,10 +10,12 @@ class LandmarkSensor:
         self.fov = kwargs.pop('fov', 2 * math.pi)
         self.maxdist = kwargs.pop('maxdist', np.finfo(np.float32).max)
         self.measure = kwargs.pop('measure', 'position')
+        self.obstacles = kwargs.pop('obstacles', None)
         self.landmarks = transforms.hom(landmarks)
 
     def sense(self, robot, **kwargs):        
         sense_err = kwargs.pop('err', self.sense_err)
+        obstacles = kwargs.pop('obstacles', self.obstacles)
         
         # Transform into robot space
         tl = np.dot(robot.world_in_robot(), self.landmarks)
@@ -26,9 +28,23 @@ class LandmarkSensor:
         e *= sigma[np.newaxis, :]
         tl += e
 
+        # Compute angles w.r.t landmarks in -pi,pi
         angles = np.arctan2(tl[1], tl[0])
 
+        # Determine seen / unseen elements
         mask = np.logical_and(dists <= self.maxdist, np.abs(angles) <= self.fov / 2)
+
+        # If obstacles are provided determine occluded landmarks via ray-tracing
+        if obstacles:
+            o = robot.state[:2]
+            for i, m in enumerate(mask):
+                if not m:
+                    continue                    
+                d = self.landmarks[:2, i] - o
+                n = np.linalg.norm(d)
+                d /= n
+                ret, t, cell = obstacles.intersect_with_ray(o, d, tmax=n)
+                mask[i] = t > n
 
         if self.measure == 'position':
             return mask, tl

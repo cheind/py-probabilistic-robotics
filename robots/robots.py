f4787ec17f6a8ce53558e3ad59e8a37f2ba72411
Christoph Heindl <christoph.heindl@gmail.com>
2017-02-27 19:48:13 +0100

    Added docs

--- robots/robots.py
+++ robots/robots.py
@@ -6,15 +6,33 @@ from robots.posenode import PoseNode
 
 
 class XYPhiRobot(PoseNode):
+    """A robot determined by its position in x,y and heading phi."""
 
     def __init__(self, **kwargs):        
+        """Create a XYPhiRobot.
+
+        Kwargs
+            err : 1x2 vector describing the error associated with motion. The error model assumes
+                  a zero centered normal distribution having a standard deviation proportional to
+                  [error per radians, error per meter].
+            pose : (optional) 1x3 pose vector. If omitted identity is assumed.
+        """
         self.motion_err = np.array(kwargs.pop('err', [0., 0.]), dtype=float)
 
         pose = np.array(kwargs.pop('pose', [0.,0.,0.]), dtype=float)
         super(XYPhiRobot, self).__init__(pose=pose)
 
     def move(self, motion, **kwargs):
-        """Move robot by first turning and then driving along the robot's x-axis."""
+        """Move robot by first turning and then driving along the robot's new heading (x-axis).
+
+        Params
+            motion : 1x2 motion vector [angle in radians, meter]          
+              
+        Kwargs
+            err : 1x2 vector describing the error associated with motion. The error model assumes
+                  a zero centered normal distribution having a standard deviation proportional to
+                  [error per radians, error per meter].
+        """
         motion_err = kwargs.pop('err', self.motion_err)
         
         sigma = motion_err * np.abs(motion)

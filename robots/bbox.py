7eda4f0ab642afb901d7dd03aee767737ed5d1ff
Christoph Heindl <christoph.heindl@gmail.com>
2017-02-27 19:26:15 +0100

    updating docs

--- robots/bbox.py
+++ robots/bbox.py
@@ -3,12 +3,23 @@ import numpy as np
 from robots.posenode import PoseNode
 
 def safe_invdir(d):
+    """Returns `1/d` with special care about zero values in `d`."""
     with np.errstate(divide='ignore'):
         return np.where(d == 0., 1e12, 1./d)
 
 class BBox(PoseNode):
+    """A positional node representing an axis aligned box."""
 
     def __init__(self, mincorner, maxcorner, **kwargs):
+        """Create a BBox.
+
+        Params
+            mincorner : 1x2 minimum corner of box
+            maxcorner : 1x2 maximum corner of box
+
+        Kwargs
+            pose : (optional) 1x3 pose vector. If omitted identity is assumed.
+        """
         self.bounds = np.column_stack((mincorner, maxcorner)).astype(float)
 
         pose = np.array(kwargs.pop('pose', [0.,0.,0.]), dtype=float)
@@ -16,15 +27,26 @@ class BBox(PoseNode):
         
     @property
     def mincorner(self):
+        """Returns the 1x2 min-corner of the box."""
         return self.bounds[:, 0]
 
     @property
     def maxcorner(self):
+        """Returns the 1x2 max-corner of the box."""
         return self.bounds[:, 1]
 
     def intersect_with_ray(self, o, d):
-        #http://gamedev.stackexchange.com/questions/18436/most-efficient-aabb-vs-ray-collision-algorithms
+        """Intersects box with ray.
 
+        Params
+            o: 1x3 ray orgin in the coordinate frame of the box.
+            d: 1x3 unit length ray direction in the coordinate frame of the box.
+
+        Returns
+            ret: boolean value indicating whether the ray hit the box or not.
+            tmin: parametric ray time of ray entering box.
+            tmax: parametric ray time of ray exiting box.
+        """
         invd = safe_invdir(d)
         low = (self.mincorner - o) * invd
         high = (self.maxcorner - o) * invd

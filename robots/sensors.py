deb6b4bd70c1be46dd7d67076645c29d30d842df
Christoph Heindl <christoph.heindl@gmail.com>
2017-02-27 09:11:23 +0100

    adding lida

--- robots/sensors.py
+++ robots/sensors.py
@@ -10,12 +10,12 @@ class LandmarkSensor:
         self.fov = kwargs.pop('fov', 2 * math.pi)
         self.maxdist = kwargs.pop('maxdist', np.finfo(np.float32).max)
         self.measure = kwargs.pop('measure', 'position')
-        self.obstacles = kwargs.pop('obstacles', None)
+        self.environment = kwargs.pop('environment', None)
         self.landmarks = transforms.h(landmarks)
 
     def sense(self, robot, **kwargs):        
         sense_err = kwargs.pop('err', self.sense_err)
-        obstacles = kwargs.pop('obstacles', self.obstacles)
+        environment = kwargs.pop('environment', self.environment)
         measure = kwargs.pop('measure', self.measure)
         
         # Transform into robot space
@@ -37,7 +37,7 @@ class LandmarkSensor:
         mask = np.logical_and(dists <= self.maxdist, np.abs(angles) <= self.fov / 2)
 
         # If obstacles are provided determine occluded landmarks via ray-tracing
-        if obstacles:
+        if environment:
             o = robot.pose[:2]
             for i, m in enumerate(mask):
                 if not m:
@@ -45,7 +45,7 @@ class LandmarkSensor:
                 d = self.landmarks[:2, i] - o
                 n = np.linalg.norm(d)
                 d /= n
-                ret, t, cell = obstacles.intersect_with_ray(o, d, tmax=n)
+                ret, t, cell = environment.intersect_with_ray(o, d, tmax=n)
                 mask[i] = t > n
 
         if measure == 'position':
@@ -58,3 +58,32 @@ class LandmarkSensor:
         else:
             raise ValueError('Unknown measure type')
         
+
+class LidarSensor:
+
+    def __init__(self, environment, **kwargs):
+        self.environment = environment
+        self.maxdist = kwargs.pop('maxdist', np.finfo(np.float32).max)
+        
+        self.fov = kwargs.pop('fov', 2 * math.pi)
+        self.angular_res = kwargs.pop('angular_resolution', 0.1)        
+        self.angles = np.arange(-self.fov/2, self.fov/2, self.angular_res)
+        self.sense_err = kwargs.pop('err', 0)
+
+    def sense(self, robot, **kwargs):
+        sense_err = kwargs.pop('err', self.sense_err)
+        environment = kwargs.pop('environment', self.environment)
+
+        mask = np.zeros(len(self.angles), dtype=bool)
+        points = np.zeros((2, len(self.angles)))
+
+        pose = robot.pose
+        for i, a in enumerate(self.angles):
+            d = np.array([math.cos(pose[2] - a), math.sin(pose[2] - a)])
+            ret, t, cell = environment.intersect_with_ray(pose[:2], d, tmax=self.maxdist)
+            if ret:
+                points[:, i] = pose[:2] + t * d
+                mask[i] = True
+
+        points = transforms.transform(robot.world_in_robot, points)
+        return mask, points

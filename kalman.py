cf9d7917b299b16c04f300b908aa69d55fdf1a45
Thomas Pönitz <thomas.poenitz@profactor.at>
2017-02-28 10:12:53 +0100

    bug in sensor?

--- kalman.py
+++ kalman.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import math
 
+np.set_printoptions(linewidth=180)
+
 class Kalman:
     def __init__(self, true_state, x, P):
         self.true_state = true_state.copy()    
@@ -43,21 +45,25 @@ class Kalman:
         z = landmark_pos.reshape((-1, 1))
         
         lm = landmark_mask.copy()
-        lm = np.hstack((lm, lm)).reshape((-1,))
+        lm = np.vstack((lm, lm)).reshape(-1, order='F')
         H = self.H[lm, :]
         
         y = z - H.dot(self.x)
-
-        S = self.H.dot(self.P).dot(self.H.T) + self.R
         
-        K = self.P.dot(self.H.T).dot(np.linalg.inv(S))
-
-        # hmmm??
-        yy = np.zeros((self.H.shape[0], 1))
-        yy[lm] = y
+        S = H.dot(self.P).dot(H.T) + self.R[lm, lm]
+                
+        try:
+            invS = np.linalg.inv(S)
+        except:
+            print(np.hstack((H.dot(self.x), z)))
+            #import sys
+            #sys.exit(0)
+            return
+               
+        K = self.P.dot(H.T).dot(invS)
         
-        self.x = self.x + K.dot(yy)
-        self.P = (np.eye(self.P.shape[0]) - K.dot(self.H)).dot(self.P)
+        self.x = self.x + K.dot(y)
+        self.P = (np.eye(self.P.shape[0]) - K.dot(H)).dot(self.P)
         
     def true_measurement(self):
         ts = np.repeat(self.true_state[:2], (self.x.shape[0] - 2) / 2, 1)
@@ -72,20 +78,22 @@ if __name__ == '__main__':
     np.random.seed(0)
     
     # Landmarks in world space
-    landmarks = np.random.uniform(0.0, 100.0, [2, 100])
+    landmarks = np.vstack((
+        np.random.uniform(0.0, 100.0, [1, 10]),
+        np.random.uniform(0.0, 20.0, [1, 10])))
 
     # Virtual x,y,phi robot
-    robot = XYRobot(pose=[50,50,np.pi / 2.0], err=0.) 
+    robot = XYRobot(pose=[50,10,0], err=0.0) 
     world['robot'] = robot  
 
     # Virtual sensor reporting bearings in robot space. Detectable landmarks are limited by FOV, max-dist and obstacles
-    sensor = LandmarkSensor(landmarks, err=0.01, fov=1.8 * math.pi, maxdist=10., measure='bearing', environment=world)
+    sensor = LandmarkSensor(landmarks, err=0.0, fov=2.0 * math.pi, maxdist=100., measure='bearing')
     world['robot']['sensor'] = sensor
 
     drawer = Drawer()
     fig, ax = plt.subplots()
-    ax.set_xlim([0, 100])
-    ax.set_ylim([0, 100])
+    ax.set_xlim([-20, 120])
+    ax.set_ylim([-20, 40])
     ax.set_aspect('equal')
     ax.grid()
     
@@ -96,7 +104,7 @@ if __name__ == '__main__':
     P = np.eye(x.shape[0])
     P[2, 2] = 0.0
     P[3, 3] = 0.0
-    k = Kalman(true_state, x, P)
+    k = Kalman(true_state, true_state, P)
     
     drawer.draw_grid(world, ax, alpha=0.5)
     
@@ -113,18 +121,18 @@ if __name__ == '__main__':
                 u[0] = f(90 - robot.pose[0])
                 u[1] = f(10 - robot.pose[1])
                 yield u
-            while robot.pose[1] < 90:
-                u[0] = f(90 - robot.pose[0])
-                u[1] = f(90 - robot.pose[1])
-                yield u
+            #while robot.pose[1] < 90:
+            #    u[0] = f(90 - robot.pose[0])
+            #    u[1] = f(90 - robot.pose[1])
+            #    yield u
             while robot.pose[0] > 10:
-                u[0] = f(10 - robot.pose[0])
-                u[1] = f(90 - robot.pose[1])
-                yield u
-            while robot.pose[1] > 10:
                 u[0] = f(10 - robot.pose[0])
                 u[1] = f(10 - robot.pose[1])
                 yield u
+            #while robot.pose[1] > 10:
+            #    u[0] = f(10 - robot.pose[0])
+            #    u[1] = f(10 - robot.pose[1])
+            #    yield u
     movement_gen = movement()
     
     def update(i):
@@ -134,9 +142,10 @@ if __name__ == '__main__':
         k.predict(m)
         
         landmark_mask, landmark_pos = sensor.sense(measure='position')
-        landmark_pos = landmark_pos.T[landmark_mask].T
+        print(landmark_mask)
+        landmark_pos = landmark_pos[:, landmark_mask]
         #landmark_indices = np.where(landmark_mask)[0]
-        if np.sum(landmark_mask):
+        if np.sum(landmark_mask) > 2:
             k.update(landmark_mask, landmark_pos)        
 
         # First sensor
@@ -148,7 +157,7 @@ if __name__ == '__main__':
         
         landmarks2 = k.x[2:].reshape((2, -1))
         
-        u += drawer.draw_points(landmarks2, ax, fc='r')
+        u += drawer.draw_points(landmarks2, ax, fc='r', zorder=10, marker='.')
 
         return u
 

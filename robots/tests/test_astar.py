0666cb3f904358fa86ff8c2488f77d489a58576a
Christoph Heindl <christoph.heindl@gmail.com>
2017-03-04 18:25:16 +0100

    graph is responsible for providing heuristic and cost.

--- robots/tests/test_astar.py
+++ robots/tests/test_astar.py
@@ -10,28 +10,51 @@ def test_gridgraph():
     mask = np.zeros((10,10))
     mask[5, 5] = 1.
     grid = Grid(mask, [0,0], [10,10])
-    graph = GridGraph(grid, GridGraph.FourN)
+    graph = GridGraph(grid, lambda a,b: 1., lambda a,b: 0.)
 
     np.testing.assert_allclose(graph.neighbors([0,0]), [[1, 0], [0, 1]])
     np.testing.assert_allclose(graph.neighbors([9,9]), [[9, 8], [8, 9]])
     np.testing.assert_allclose(graph.neighbors([5,4]), [[5, 3], [6,4], [4,4]])
-
-    assert graph.cost((0,0), (0,1)) == 1.
 
 def test_astar():
 
     mask = np.array([
         [0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 1, 0],
-        [0, 1, 0, 1, 1, 0],
-        [0, 0, 0, 0, 1, 0]
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
     ])
     grid = Grid(mask, [0,0], [6,5])
-    graph = GridGraph(grid, GridGraph.FourN)
+    graph = GridGraph(grid, lambda a,b: 1., lambda a,b: 0.)
+
+    path = astar((0, 0), (4, 4), graph)
+    np.testing.assert_allclose(path, [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [0, 4],
+        [1, 4],
+        [2, 4],
+        [3, 4],
+        [4, 4]
+    ])
+
+def test_astar_nopath():
+
+    mask = np.array([
+        [0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ])
+    grid = Grid(mask, [0,0], [6,5])
+    graph = GridGraph(grid, lambda a,b: 1., lambda a,b: 0.)
 
     def heuristic(node, goal):
         return 0.
 
-    path = astar((0, 0), (5, 3), graph, heuristic)
-    print(path)
+    path = astar((0, 0), (2, 0), graph)
+    np.testing.assert_allclose(path, [])

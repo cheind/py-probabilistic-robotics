77436440ff85f01cb0ac8358e886a3275c9b5240
Christoph Heindl <christoph.heindl@gmail.com>
2017-03-13 21:13:56 +0100

    supports anticipating velocities for less oscillations.
    does not yet support multi-axis correctly.

--- robots/planning/trajectories/trapezoidal.py
+++ robots/planning/trajectories/trapezoidal.py
@@ -3,7 +3,7 @@ import math
 
 class TrapezoidalTrajectory:
 
-    def __init__(self, q, dq_max, ddq_max):
+    def __init__(self, q, dq_max, ddq_max, dq=None):
 
         assert len(q) > 1
 
@@ -16,11 +16,28 @@ class TrapezoidalTrajectory:
         self.dt = np.zeros(self.nseg)
         self.t = np.zeros(self.nseg+1)
         self.ta = np.zeros(self.nseg)
-        self.v = np.zeros((self.nseg, self.q.shape[1]))
+        self.td = np.zeros(self.nseg)
+        self.v = np.zeros((self.nseg, self.q.shape[1]))        
         self.a = np.zeros((self.nseg, self.q.shape[1]))
 
+        
+        
+        if dq is None:            
+            # Initial / final velocities zero, others according to sign of velocity in
+            # neighboring segments.
+            self.v0 = np.zeros(self.q.shape)
+            s = np.sign(np.diff(self.q, axis=0))
+            for i in range(1, len(q)-1):
+                if s[i-1] == s[i]:
+                    self.v0[i] = s[i-1] * dq_max        
+        else:
+            self.v0 = np.asarray(dq)            
+            if self.v0.ndim == 1:
+                self.v0 = self.v0.reshape(-1, 1)
+            assert self.v0.shape == (len(q), 1) or self.v0.shape == self.q.shape
+
         for i in range(self.nseg):
-            self.dt[i], self.ta[i], self.v[i], self.a[i] = TrapezoidalTrajectory.solve(q[i], q[i+1], dq_max, ddq_max)
+            self.dt[i], self.ta[i], self.td[i], self.v[i], self.a[i] = TrapezoidalTrajectory.solve(self.q[i], self.q[i+1], self.v0[i], self.v0[i+1], dq_max, ddq_max)
 
         self.t[1:] = np.cumsum(self.dt)
 
@@ -53,22 +70,22 @@ class TrapezoidalTrajectory:
         t = t - self.t[i] # Time relative to segment
         t = t.reshape(-1, 1) # Needed for elementwise ops
         ta = self.ta.reshape(-1, 1)
+        td = self.td.reshape(-1, 1)
         dt = self.dt.reshape(-1, 1)
 
-
-        isa = t <= ta[i]
-        isd = t > (dt[i] - ta[i])
+        isa = t < ta[i]
+        isd = t > (dt[i] - td[i])
         isl = np.logical_and(~isa, ~isd)
 
         q = \
-            (self.q[i] + 0.5*self.a[i]*t**2) * isa + \
-            (self.q[i] + self.v[i]*(t - ta[i]*0.5)) * isl + \
-            (self.q[i+1] - 0.5*self.a[i]*(dt[i]-t)**2) * isd
+            (self.q[i] + self.v0[i]*t + 0.5*self.a[i]*t**2) * isa + \
+            (self.q[i] + self.v0[i]*ta[i]*0.5 + self.v[i]*(t - ta[i]*0.5)) * isl + \
+            (self.q[i+1] - self.v0[i+1]*(dt[i]-t) - 0.5*self.a[i]*(dt[i]-t)**2) * isd
 
         dq = \
-            (self.a[i] * t) * isa + \
+            (self.v0[i] + self.a[i] * t) * isa + \
             self.v[i] * isl + \
-            (self.a[i] * (dt[i]-t)) * isd
+            (self.v0[i+1] + self.a[i] * (dt[i]-t)) * isd
 
         ddq = \
             self.a[i] * isa + \
@@ -79,10 +96,12 @@ class TrapezoidalTrajectory:
         return (q, dq, ddq) if not isscalar else (q[0], dq[0], ddq[0])
 
     @staticmethod
-    def solve(q0, q1, dq_max, ddq_max):
+    def solve(q0, q1, dq0, dq1, dq_max, ddq_max):
 
         q0 = np.atleast_1d(q0)
         q1 = np.atleast_1d(q1)
+        dq0 = np.abs(np.atleast_1d(dq0))
+        dq1 = np.abs(np.atleast_1d(dq1))
 
         # For coordinated motion, timing is build for axis with largest displacement
         d = q1 - q0
@@ -90,6 +109,25 @@ class TrapezoidalTrajectory:
         s = np.sign(d)
         i = np.argmax(h)
 
+        assert ddq_max*h >= np.abs(dq0**2 - dq1**2) * 0.5, "Trajectory with given constraints impossible. Lower velocities or increase maximum acceleration."
+
+        if h[i]*ddq_max > dq_max**2 - (dq0[i]**2 + dq1[i]**2) * 0.5:
+            # vmax is reached
+            vlim = dq_max
+            ta = (dq_max - dq0[i]) / ddq_max
+            td = (dq_max - dq1[i]) / ddq_max
+            t = h[i] / dq_max + (dq_max / (2 * ddq_max)) * (1 - dq0[i]/dq_max)**2 + (dq_max / (2 * ddq_max)) * (1 - dq1[i]/dq_max)**2
+        else:
+            # vmax is not reached, only accel and deaccel
+            vlim = math.sqrt(h[i]*ddq_max + (dq0[i]**2 + dq1[i]**2) * 0.5)
+            ta = (vlim - dq0[i]) / ddq_max
+            td = (vlim - dq1[i]) / ddq_max
+            t = ta + td
+
+        dq = s * vlim
+        ddq = s * ddq_max
+        
+        """
         ta = dq_max / ddq_max # Acceleration time
         t = (h[i] * ddq_max + dq_max**2) / (ddq_max * dq_max) # Total time
 
@@ -104,8 +142,9 @@ class TrapezoidalTrajectory:
 
         dq[i] = s[i] * dq_max
         ddq[i] = s[i] * ddq_max
+        """
 
-        return t, ta, dq, ddq
+        return t, ta, td, dq, ddq
 
 
 
@@ -114,3 +153,4 @@ class TrapezoidalTrajectory:
 
 
         
+
--- robots/tests/planning/trajectories/test_trapezoidal.py
+++ robots/tests/planning/trajectories/test_trapezoidal.py
@@ -17,9 +17,18 @@ def test_multi_segment1():
     # Zero velocities at sample points
     # No coasting as velocity of 20 is never reached
 
-    traj = TrapezoidalTrajectory([0, 20, 50, 40, 10], 20, 10)
-    x, dx, ddx = traj(traj.t)
+    traj = TrapezoidalTrajectory([0, 20, 50, 40, 10], 15, 20)
+    
+    t = np.linspace(0, traj.t[-1], 1000)
+    plot_traj(t, *traj(t))
 
+    x, dx, ddx = traj(traj.t)
+    plt.scatter(traj.t, x[:, 0])
+
+    plt.show()
+
+    
     np.testing.assert_allclose(x[:, 0], [0, 20, 50, 40, 10])
-    np.testing.assert_allclose(dx[:, 0], 0, atol=1e-4)
-    np.testing.assert_allclose(ddx[:, 0], [10, 10, -10, -10, 10])
+    np.testing.assert_allclose(dx[:, 0], [0, 15, 0, -15, 0], atol=1e-4)
+    np.testing.assert_allclose(ddx[:, 0], [20, 0, -20, 0, 20])
+    
